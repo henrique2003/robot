f4ef9ea2cea8c8835bfd4fd2f45b68e79dd534d3
henrique2003 <henrique.de.melo.cristioglu@gmail.com>
2021-10-03 22:08:02 -0300

    feat: create express server

--- dist/server.js
+++ dist/server.js
@@ -3,5 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = require("dotenv");
 const bot_1 = __importDefault(require("./bot"));
+(0, dotenv_1.config)();
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.send('Bot on!');
+});
+app.listen(process.env.PORT, () => {
+    console.log('Server running');
+});
 bot_1.default;
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,18 @@
+import express, { Response, Request } from 'express'
+import { config } from 'dotenv'
+
 import Bot from './bot'
+
+config()
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Bot on!')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log('Server running')
+})
 
 Bot
